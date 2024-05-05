1f92a241e86ac9f7b526a3fdf9423500db4ac16c
joevtap <joelvitortorres@gmail.com>
2024-05-05 14:36:51 -0300

    fix: using env for allowed origins

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,12 @@ const app = express();
 
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: [...(process.env.ALLOW_ORIGINS ?? "").split(",")],
+  }),
+);
 app.use(cookieParser());
 
 app.use("/", router);
